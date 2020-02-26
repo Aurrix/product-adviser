23e878155608c65ec367b7e99ee935a481b21bba
Alisher <urunov.alisher97@gmail.com>
2020-02-26 21:26:31 +0200

    CORS disabled

--- src/main/java/se/seb/productadviser/global/config/WebConfig.java
+++ src/main/java/se/seb/productadviser/global/config/WebConfig.java
@@ -7,13 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
   @Bean
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedHeaders("Authorization", "Cache-Control", "Content-Type", "Accept",
+                "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
+                "Origin")
+            .exposedHeaders("Access-Control-Expose-Headers", "Authorization", "Cache-Control",
+                "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers",
+                "Origin")
+            .allowedMethods("PUT", "GET", "POST", "DELETE", "OPTIONS");
       }
     };
   }
